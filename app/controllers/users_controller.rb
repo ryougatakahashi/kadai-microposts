3e0009cdd77e71baf5fa0eb19e60cd5393c14b78
ryouga takahashi <ryouga201215@yahoo.co.jp>
2023-04-12 06:35:51 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-   before_action :require_user_logged_in, only: [:index, :show]
-  
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
   def index
     @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
